bf88fab3cdf1b6da6c098bf96ebd6fc76d90bea8
Marc Nelson B. Ochavo <89735506+n3wt0n03@users.noreply.github.com>
2023-09-17 05:56:31 -0400

    Update BasePlusCommisionEmployee.java

--- EmployeeProject/src/employee/version1/BasePlusCommisionEmployee.java
+++ EmployeeProject/src/employee/version1/BasePlusCommisionEmployee.java
@@ -1,3 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee.version1;
+
+/**
+ *
+ * @author User
+ */
 public class BasePlusCommissionEmployee {
     private int empID = 0;
     private String empName = "";
