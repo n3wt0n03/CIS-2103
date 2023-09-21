2399cad683d6c1bfd3c30c6fb386aecaa531aa97
Marc Nelson B. Ochavo <marcnelsonochavo@gmail.com>
2023-09-22 00:50:15 +0800

    Employee Version 2

---
+++ EmployeeProject/src/employee/verison2/Employee.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employee.verison2;
+
+/**
+ *
+ * @author NewtskieTzy
+ */
+public class Employee {
+    private int empID = 0;
+    private String empName = "";
+    private Date empDateHired = new Date(1, 1, 2023);
+    private Date empBirthDate = new Date(1, 1, 2023); 
+
+    public Employee() {
+        
+    }
+    
+     public Employee(int empID, String empName, Date empDateHired, Date empBirthDate) {
+        this.empID = empID;
+        this.empName = empName;
+        this.empDateHired = empDateHired;
+        this.empBirthDate = empBirthDate;
+    }
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public Date getEmpDateHired() {
+        return empDateHired;
+    }
+
+    public void setEmpDateHired(Date empDateHired) {
+        this.empDateHired = empDateHired;
+    }
+
+    public Date getEmpBirthDate() {
+        return empBirthDate;
+    }
+
+    public void setEmpBirthDate(Date empBirthDate) {
+        this.empBirthDate = empBirthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: \n" + empID + 
+               "Name: \n" + empName + 
+               "DateHired: \n" + empDateHired + 
+               "BirthDate: \n" + empBirthDate;
+    }
+    
+    
+    
+}
